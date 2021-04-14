191b03f83b59a870077c6382e52f4762840c926d
kennja05 <jacobkenny05@gmail.com>
2021-04-13 23:18:34 -0400

    add mp3 files

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,4 +2,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
     //scriptssss
     //TODO
     //director password
+    function playMusic(){
+        const sound = new Howl({
+            src: ['audio/instantConcert.mp3']
+        })
+        sound.play()
+    }
+    const div = document.getElementById('audio')
+    div.addEventListener('click', () => {
+        console.log('shit')
+        playMusic()
+    })
 })
